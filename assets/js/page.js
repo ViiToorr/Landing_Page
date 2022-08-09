71b6581818f440230f3880c29daa48be13f7ccf6
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-08-09 10:07:31 -0300

    js

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,16 +1,8 @@
-function escrever (el) {
 
-        let textoArray = el.innerHTML.split("Programador ");
-        el.innerHTML = " ";
-
-        textoArray.forEach(function(texto) {
-            el.innerHTML += `<span>${texto}</span>`;
-            setTimeout.innerHTML += "Programador ";
-        }
-    );
-
-
-}
-
-var titulo = document.querySelector('h5');
-escrever(titulo);
+document.addEventListener('DOMContentLoaded', () => {
+    new TypeIt(".animacao",
+    {
+        speed : 250,
+        strings: "JavaScript"
+    }).go();
+})
