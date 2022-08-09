6f2d7992e2ae4326065f8d0c89f0f7086a9766ba
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-08-09 09:50:03 -0300

    teste

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,1 +1,16 @@
-alert(`ola`)
+function escrever (el) {
+
+        let textoArray = el.innerHTML.split("Programador ");
+        el.innerHTML = " ";
+
+        textoArray.forEach(function(texto) {
+            el.innerHTML += `<span>${texto}</span>`;
+            setTimeout.innerHTML += "Programador ";
+        }
+    );
+
+
+}
+
+var titulo = document.querySelector('h5');
+escrever(titulo);
