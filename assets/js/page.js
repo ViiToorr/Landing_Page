8697d5b085b8c1d6caca4bc013bfe58b6291247c
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-08-13 09:50:10 -0300

    maquina

---
+++ assets/js/page.js
@@ -0,0 +1,13 @@
+function escrever(elemento){
+    const textoArray = elemento.innerHTML.split(``);
+    elemento.innerHTML = ` `;
+    textoArray.forEach((letra, i) => {   
+      
+    setTimeout(() => {
+        elemento.innerHTML += letra;
+    }, 150 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+escrever(titulo);
